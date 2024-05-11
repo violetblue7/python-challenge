88ff74b4fc50955d0ed15c387b98b4055711ceb2
Lina Blanco <linablanco@live.com>
2024-05-11 17:53:50 -0400

    Results

--- PyBank/analysis/Main.py
+++ PyBank/analysis/Main.py
@@ -2,7 +2,7 @@
 import csv
 
 # File path
-csvpath = "PyBank/Resources/budget_data.csv"
+csvpath = "python-challenge/PyBank/Resources/budget_data.csv"
 
 # Call the funtion to get the total number of rows for months using built-in methods 
 def count_rows (data):
@@ -17,8 +17,8 @@ with open(csvpath, encoding= 'UTF-8') as csvfile:
     total_months = 0
     net_total = 0
     changes = []
-    greatest_increase = 0
-    greatest_decrease = 0
+    max_increase = 0
+    min_decrease = 0
 
     # Skip the header row
     next(csvreader)
@@ -33,20 +33,20 @@ with open(csvpath, encoding= 'UTF-8') as csvfile:
             change = int(row[1]) - int(prev_row[1])
             changes.append(change)
             
-            if change > greatest_increase:
-                greatest_increase = change
-                greatest_increase_date = row[0]
-            if change < greatest_decrease:
-                greatest_decrease = change
-                greatest_decrease_date = row[0]
+            if change > max_increase:
+                max_increase = change
+                max_increase_date = row[0]
+            if change < min_decrease:
+                min_decrease = change
+                min_decrease_date = row[0]
 
-        prev_row = row  # Store the current row for the next iteration
+        prev_row = row  # find current row for the next step
 
 
 # Calculate the average change
 average_change = sum(changes) / len(changes) 
 
-        # Other formats to present the outcome with code explained
+        # Other formats to present the outcome with code explained per Xpert source
         # print("Financial Analysis")
         # print("----------------------------")
         # print(f"Total Months: {total_months}")
@@ -68,13 +68,13 @@ Total: ${net_total}
 
 Average Change: ${average_change:.2f}
 
-Greatest Increase in Profits: {greatest_increase_date} (${greatest_increase})
+Greatest Increase in Profits: {max_increase_date} (${max_increase})
 
-Greatest Decrease in Profits: {greatest_decrease_date} (${greatest_decrease})
+Greatest Decrease in Profits: {min_decrease_date} (${min_decrease})
 """
 # Print the output
 print(output)
 
 # Write the output to a text file
-with open("PyBank/analysis/pybank.txt", 'w') as f:
+with open("python-challenge/PyBank/analysis/pybank.txt", 'w') as f:
     f.write(output)
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # python-challenge
+
+Learning about Python was an eye opening experience, now I understand the resistance that the industry has on VBA. Python's funtionality using csv reader, encoding "UTF-8", casting integers, f-string method, working thru lists and dictionaries. Making the process flow understable. 
+
+Functions and transactions used in the exercices:
+Conditionals (if)
+Logic tests (<>, ==)
+Keys and values
+List methods: append, index and len
+Interate over the data, use loops (while). 
+
+References: 
+Bootcamp: DATA-PT-EAST-APRIL-041524-MTTH Xpert Learning Assistant.
+Module 3: Python Class Exercises . Professor Alexander Booth
